2f9455be923c3e9cc9dde686ea0d980bbb5ca0a6
Gary <gary@roomworksmedia.com>
2022-06-23 08:02:15 +0100

    set new game at start and display number of turns

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-import SingleCard  from './components/SingleCard'
+import SingleCard from './components/SingleCard'
 
 const cardImages = [
   {
     "src": "/img/helmet-1.png",
-    matched : false,
+    matched: false,
   },
   {
     "src": "/img/potion-1.png",
-    matched : false,
+    matched: false,
   },
   {
     "src": "/img/ring-1.png",
-    matched : false,
+    matched: false,
   },
   {
     "src": "/img/scroll-1.png",
-    matched : false,
+    matched: false,
   },
   {
     "src": "/img/shield-1.png",
-    matched : false,
+    matched: false,
   },
   {
     "src": "/img/sword-1.png",
-    matched : false,
+    matched: false,
   }
 ]
 
@@ -37,13 +37,15 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setchoiceOne] = useState(null)
   const [choiceTwo, setchoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
   //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card, index) => ({ ...card, id: Math.random() }))
-
+    setchoiceOne(null)
+    setchoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
   }
@@ -57,48 +59,55 @@ function App() {
   // comapre 2 selected cards with useEffect
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true)
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
             if (card.src === choiceOne.src) {
               return { ...card, matched: true }
             } else {
-            return card
+              return card
             }
-            
+
           })
         })
         resetTurn()
-    } else {
-      setTimeout(() => resetTurn(), 1000)
-      
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+
+      }
     }
-  }
 
   }, [choiceOne, choiceTwo])
 
-  console.log(cards)
 
   const resetTurn = () => {
     setchoiceOne(null)
     setchoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
+  
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
-      {cards.map(card => (
-          <SingleCard 
-          card={card}  
-          key={card.id} 
-          handleChoice={handleChoice}
-          flipped={card === choiceOne || card === choiceTwo || card.matched}
+        {cards.map(card => (
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
-          ))}
+        ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -2,22 +2,24 @@ import React from 'react'
 
 import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice, flipped}) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
-const handleClick = () => {
-    handleChoice(card)
-}
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+    }
 
     return (
 
         <div className="card">
             <div className={flipped ? "flipped" : ""}>
                 <img className='front' src={card.src} alt="card front" />
-                <img 
-                className='back' 
-                src='/img/cover.png' 
-                alt="card back" 
-                onClick={handleClick}/>
+                <img
+                    className='back'
+                    src='/img/cover.png'
+                    alt="card back"
+                    onClick={handleClick} />
             </div>
 
         </div>
